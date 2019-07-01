d36b7865ef5bba974d9d14f773e962d6f93b8b67
Jeremy Reyes B <jjrb6@hotmail.com>
2019-07-01 08:52:55 -0500

    [feat]: Added seeder user.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $options = ['cost' => 13];
+
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'Jeremy Reyes B.',
+            'email' => 'jjrb6@hotmail.com',
+            'email_verified_at' => date('Y-m-d H:i:s'),
+            'password' => password_hash("123", PASSWORD_BCRYPT, $options),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+
+        DB::table('users')->insert([
+            'id' => 2,
+            'name' => 'Club House Admin',
+            'email' => 'juan@club-house.com',
+            'email_verified_at' => date('Y-m-d H:i:s'),
+            'password' => password_hash("ClubH0us3.", PASSWORD_BCRYPT, $options),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
