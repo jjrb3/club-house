ac183440586926ada5d2b2fd42a39e71d547a70d
Jeremy Reyes B <jjrb6@hotmail.com>
2019-07-07 11:11:13 -0500

    [design]: Added style to categories.

--- si/resources/views/category/index.blade.php
+++ si/resources/views/category/index.blade.php
@@ -1,59 +1,74 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        Listado de categorias
-                    </div>
 
-                    <div class="card-body">
+    <div class="row bg-title">
+        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+            <h4 class="page-title">Categoria de productos</h4> </div>
+        <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+            <ol class="breadcrumb">
+                <li><a href="{{ route('category') }}">Categoria</a></li>
+            </ol>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <h3 class="box-title">Listado de categorias</h3>
+                Para agregar una nueva categoria presione <a href="{{ route('category.create.index') }}">AQUÍ</a>. Al
+                momento de actualizar una categoria podrá ver el listado de productos asociados a esa categoria.
+                <br>
+                <br>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Icono</th>
+                        <th scope="col">Opciones</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>CLUB POTATOES</td>
+                        <td>Papas a la francesa, papas en casco, queso americano, beicon, sour crearm</td>
+                        <td align="center"><i class="fa fa-cutlery" aria-hidden="true"></i></td>
+                        <td>
+                            <a>
+                                <button class="btn btn-default">
+                                    Editar
+                                </button>
+                            </a>
+                            <a>
+                                <button class="btn btn-danger">
+                                    Eliminar
+                                </button>
+                            </a>
 
-                        <a href="{{ route('category.create.index') }}">
-                            <button class="btn btn-primary">
-                                Agregar nueva categoria
-                            </button>
-                        </a>
-                        <br>
-                        <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>CLUB POTATOES</td>
+                        <td>Papas a la francesa, papas en casco, queso americano, beicon, sour crearm</td>
+                        <td align="center"><i class="fa fa-beer" aria-hidden="true"></i></td>
+                        <td>
+                            <a>
+                                <button class="btn btn-default">
+                                    Editar
+                                </button>
+                            </a>
+                            <a>
+                                <button class="btn btn-danger">
+                                    Eliminar
+                                </button>
+                            </a>
 
-
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Descripción</th>
-                                <th scope="col">Icono</th>
-                                <th scope="col">Opciones</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>CLUB POTATOES</td>
-                                <td>Papas a la francesa, papas en casco, queso americano, beicon, sour crearm</td>
-                                <td>Comida</td>
-                                <td>
-                                    <a>
-                                        <button class="btn btn-default">
-                                            Editar
-                                        </button>
-                                    </a>
-                                    <a>
-                                        <button class="btn btn-danger">
-                                            Eliminar
-                                        </button>
-                                    </a>
-
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-
-                    </div>
-                </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+
 @endsection
